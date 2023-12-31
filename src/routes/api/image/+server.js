63b95fdf3ddad9088d44d4ec8f4d2515881472a7
dylanmas <dylanmas687@gmail.com>
2023-12-31 06:39:32 -0600

    Fixed update condition bug

--- src/routes/api/image/+server.js
+++ src/routes/api/image/+server.js
@@ -1,26 +1,26 @@
-import fs from 'fs/promises';
-import { json } from '@sveltejs/kit'
-import path from 'path';
-import { exec } from 'child_process';
+import fs from "fs/promises";
+import { json } from "@sveltejs/kit";
+import path from "path";
+import { exec } from "child_process";
 
 // Defining the path to the JSON file
 const filePath = path.join(process.cwd(), "src/lib", "preferences.json");
 
 export async function POST(request) {
   // Reading the contents of the JSON file
-  const fileContents = await fs.readFile(filePath, 'utf-8');
+  const fileContents = await fs.readFile(filePath, "utf-8");
 
   // Valid entries:
   //    frameorientation
   //    brightnessdisplay
   //    system
   //    power
-  console.log(request.url.searchParams.get('category'))
-  console.log(request.url.searchParams.get('data'))
+  console.log(request.url.searchParams.get("category"));
+  console.log(request.url.searchParams.get("data"));
 
   // Parsing the JSON data
   const data = JSON.parse(fileContents);
-  console.log(data.settingsdata.frameorientation)
+  console.log(data.settingsdata.frameorientation);
 
   switch (request.url.searchParams.get("category")) {
     case "source":
@@ -82,7 +82,7 @@ export async function POST(request) {
           return;
         }
         console.log(stdout);
-        if (!stdout.includes("Already up to date.\n")) {
+        if (!stdout.includes("Already up to date.")) {
           exec("src/lib/update.sh", (error, stdout, stderr) => {
             if (error) {
               console.log(error);
@@ -138,6 +138,5 @@ export async function POST(request) {
   const tst1 = await fs.writeFile(filePath, JSON.stringify(data));
 
   // Return the updated data
-  return json({tst1}, {status: 200})
-
+  return json({ tst1 }, { status: 200 });
 }
